fc63cad332e0634743b8e82312da4c3972287728
Parth <parthlahoti5404@gmail.com>
2023-12-23 17:56:17 +0530

    commit all changes

---
+++ first.js
@@ -0,0 +1,32 @@
+// console.log("parthkachori")
+// let a=2; let b=3;
+// console.log(a+b);
+// const student={
+//     name: "rajesh",
+//     age:87,
+//     gender:67,
+
+// };
+// console.log(student.age)
+// student.age=student.age+1;
+// console.log(student.age)
+
+const object={
+title:"ball pen",
+rating:4,
+isdeal:true,
+price:"teri aukat ke bahar",
+
+};
+
+const profile={
+    name:"Parth",
+    follow:"follow", msg:"message",
+    link:"abcd.xyz"
+};
+for(let i=0;i<5;i++){
+for(let j=0;j<i;j++){
+    console.log(" dhoni ");
+    console.log("\n");
+}
+}
